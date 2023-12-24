0644251b374ca30e937afbda84e0be2a9ead2260
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-12-24 22:53:39 +0300

    Update projectsData.tsx

--- containers/projects/data/projectsData.tsx
+++ containers/projects/data/projectsData.tsx
@@ -6,12 +6,16 @@ export const projects = [
     name: "Matbaa Hizmetlerimiz",
     badge: "Matbaa Hizmetlerimiz",
     description: ` Firmanızın kurumsal kimliğini ve prestijini ön planda tutarak yüksek kalitede üretim yapan makine parkuru ile sizlere sunulmaktadır.`,
+    githubLink: "#",
+    livePreview: "#",
     image: "https://platinumreklam.com/uploads/hizmet/matbaa-hizmeti-1570455201.jpg",
   },
   {
     name: "Reklam Hizmetleri",
     badge: "Reklam Hizmetleri",
     description: `Digital baskılı afiş ve  pankart, Işıklı ve ışıksız tabela, Kutu harf, İş güvenliği ve trafik  levhaları,  Yönlendirme Levhaları,`,
+    githubLink: "#",
+    livePreview: "#",
     image: "http://www.ankaracammutfak.com/images/sayfa/2138631560-reklam-hizmetleri.jpg",
   },
 ]
