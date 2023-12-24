cfd7df3470056a21232359f6c7d5078c2cefe68c
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-12-24 22:42:56 +0300

    Update technicalStack.tsx

--- containers/tech-stack-carousel/data/technicalStack.tsx
+++ containers/tech-stack-carousel/data/technicalStack.tsx
@@ -10,57 +10,33 @@ import { TypeScriptLogo } from "../icons/ts-logo";
 
 export const techStack = [
   {
-    name: "React",
-    usage: `JavaScript library for creating reusable components and building dynamic user interfaces with efficiency.`,
+    name: "Matbaa",
     icon: <ReactLogo className="max-md:w-6" />,
 
   },
   {
-    name: "Tailwindcss",
-    usage: "Streamlined, customizable CSS framework with pre-defined utility classes for rapid web development.",
+    name: "Reklam Ürünleri",
     icon: < TailwindLogo className="max-md:w-6" />,
 
   },
   {
-    name: "Next.js",
-    usage: "React framework offering server-side rendering, simplifying the creation of performant and SEO-friendly web applications.",
+    name: "Tabela",
     icon: <NextLogo className="max-md:w-8" />,
 
   },
   {
-    name: "TypeScript",
-    usage: "Adds static typing to JavaScript for more robust code.",
+    name: "Tasarım",
     icon: <TypeScriptLogo className="max-md:w-7" />,
 
   },
   {
-    name: "Express",
-    usage: "Lightweight Node.js framework for building web servers and APIs with simple routing and middleware.",
+    name: "Promosyon",
     icon: <ExpressLogo className="max-md:w-7" />,
 
   },
   {
-    name: "Node.js",
-    usage: "JavaScript runtime for scalable server-side applications with high performance.",
+    name: "Galeri",
     icon: <NodeLogo className="max-md:w-6" />,
-
-  },
-  {
-    name: "PostgreSQL",
-    usage: "Powerful open-source relational database for versatile applications, known for reliability and advanced features.",
-    icon: <PostgreLogo className="max-md:w-7" />,
-
-  },
-  {
-    name: "Prisma",
-    usage: "Streamlined database toolkit for efficient, type-safe queries, boosting developer productivity.",
-    icon: <PrismaLogo className="max-md:w-7" />,
-
-  },
-  {
-    name: "Astro",
-    usage: "High-performance site builder combining static and dynamic rendering for fast, SEO-friendly websites.",
-    icon: <AstroLogo className="max-md:w-7" />,
 
   },
 ];
